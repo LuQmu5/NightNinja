2f63c5920f0e66448e2d3bddebaa928c5d7b2428
LuQmu5 <135509819+LuQmu5@users.noreply.github.com>
2024-05-24 20:03:17 +0400

    Update PlayerController.cs

--- Assets/CodeBase/Player/PlayerController.cs
+++ Assets/CodeBase/Player/PlayerController.cs
@@ -1,6 +1,15 @@
 ﻿using UnityEngine;
+using Zenject;
 
 public class PlayerController : MonoBehaviour
 {
+    private PlayerInput _input;
+    private PlayerStateMachine _stateMachine;
 
+    [Inject]
+    public void Construct(PlayerInput input)
+    {
+        _input = input;
+        _stateMachine = new PlayerStateMachine();
+    }
 }
