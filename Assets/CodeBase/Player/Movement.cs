223aa6cf21658e350fdbf1d4539d40ef64910e10
LuQmu5 <135509819+LuQmu5@users.noreply.github.com>
2024-05-24 18:46:03 +0400

    1

--- Assets/CodeBase/Player/Movement.cs
+++ Assets/CodeBase/Player/Movement.cs
@@ -8,7 +8,11 @@ public class Movement : MonoBehaviour
     [SerializeField] private Rigidbody2D _rigidbody;
     [SerializeField] private float _speed = 5;
     [SerializeField] private float _jumpForce = 5;
+    [SerializeField] private LayerMask _groundMask;
+    [SerializeField] private Transform _legs;
+    [SerializeField] private float _legsRadius = 0.1f;
 
+    private bool _onGround;
     private PlayerInput _input;
 
     [Inject]
@@ -29,14 +33,21 @@ public class Movement : MonoBehaviour
         _input.Movement.Jump.started -= OnJumpStarted;
     }
 
+    private void FixedUpdate()
+    {
+        _onGround = Physics2D.OverlapCircle(_legs.position, _legsRadius, _groundMask);
+    }
+
     private void Update()
     {
         float velocityX = _input.Movement.Move.ReadValue<float>();
+
         _rigidbody.velocity = new Vector2(_speed * velocityX, _rigidbody.velocity.y);
     }
 
     private void OnJumpStarted(UnityEngine.InputSystem.InputAction.CallbackContext obj)
     {
-        _rigidbody.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
+        if (_onGround)
+            _rigidbody.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
     }
 }
